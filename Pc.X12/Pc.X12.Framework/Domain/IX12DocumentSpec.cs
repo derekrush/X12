302cb45128a5fda87db56633337abb782ba99609
derekrush <derekrush.okc@gmail.com>
2018-03-03 09:44:03 -0600

    * Pc.X12.Framework.dll: Testing commit
    * Pc.X12.userprefs:
    * project.assets.json:
    * IX12DocumentSpec.cs:
    * Pc.X12.Framework.csproj.nuget.cache:
    * Pc.X12.Framework.csproj.nuget.g.props:
    * Pc.X12.Framework.pdb:
    * Pc.X12.Framework.dll:
    * Pc.X12.Framework.pdb:
    * Pc.X12.Framework.csproj.FileListAbsolute.txt:
    * Pc.X12.Framework.csproj.CoreCompileInputs.cache:

--- Pc.X12/Pc.X12.Framework/Domain/IX12DocumentSpec.cs
+++ Pc.X12/Pc.X12.Framework/Domain/IX12DocumentSpec.cs
@@ -3,6 +3,7 @@ namespace Pc.X12.Framework.Domain
 {
     public interface IX12DocumentSpec
     {
+        //Adding comment...
         IX12DocumentDelimiterSpec DelimiterSpec { get; }
         IX12InterchangeEnvelopeSpec InterchangeEnvelopeSpec { get; }
     }
